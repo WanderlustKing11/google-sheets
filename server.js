e1c28257bd91726e0cf566432d988b9ec9d5241f
Doug <douglashorville@gmail.com>
2023-06-20 11:40:41 -0400

    Read and write from, and to, Google Sheet; render page for user to match driver name, choose response from selection, then populate sheet successfully

--- server.js
+++ server.js
@@ -6,107 +6,122 @@ const { google } = require('googleapis');
 const { log } = require('console');
 
 const app = express();
-const sheets = google.sheets('v4');
-
 app.set('view engine', 'ejs');
-
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); // enables us to use forms
 app.use('/public/css', express.static(__dirname + '/public/css'));
 
 // Google Auth //
+const authenticate = async () => {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: 'credentials.json',
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+  });
 
-// const auth = new google.auth.GoogleAuth({
-//   keyFile: 'credentials.json',
-//   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-// });
-// google.options({ auth });
-// console.log(auth);
+  // Create client instance for auth
+  const client = await auth.getClient();
+  return client;
+};
 
 // GLOBALS //
 
-// const driverNames = [
-//   'Verstappen',
-//   'Perez',
-//   'Alonso',
-//   'Hamilton',
-//   'Russell',
-//   'Stroll',
-// ];
-
-// const wankerMessage = "is a total wanker!";
-// const goatMessage = "is the GOAT!";
+const wankerMessage = "is a total wanker!";
+const goatMessage = "is the GOAT!";
 
 app.get('/', async (req, res) => {
-  const auth = new google.auth.GoogleAuth({
-    keyFile: 'credentials.json',
-    scopes: 'https://www.googleapis.com/auth/spreadsheets',
+  res.render('home', {
+    driver: null, 
+    message: null,
   });
-
-  // Creat client instance for auth
-  const client = await auth.getClient();
-
-  // Instance of Google Sheets API
-  const googleSheets = google.sheets({ version: 'v4', auth: client });
-  // const spreadsheetId = '... spreadsheetID';
-
-  // Get metadata about spreadsheet
-  const metaData = await googleSheets.spreadsheets.get({
-    auth,
-    spreadsheetId
-  });
-
-  res.send(metaData);
-  // res.render('home', {
-  //   driver: null, 
-  //   message: null,
-  // });
 });
 
-// app.post('/', (req, res) => {
-//   const typedName = req.body.drivers;
-//   let driverFound = driverNames.includes(typedName);
+app.post('/', async (req, res) => {
+  try {
+    // Authenticate and get the client
+    const client = await authenticate();
 
-//   if (driverFound) {
-//     res.redirect('/wanker?driver=' + typedName);
-//   } else {
-//     res.render('home', {
-//       driver: null,
-//       message: 'Sorry, no match found!',
-//     });
-//   }
-// });
+    // Instance of Google Sheets API
+    const googleSheets = google.sheets({ version: 'v4', auth: client });
+    // const spreadsheetId = '... spreadsheetId';
 
-// app.get('/wanker', (req, res) => {
-//   const driver = req.query.driver;
-//   if (!driver) {
-//     res.redirect('/');
-//   } else {
-//     res.render('wanker', {
-//       driver: driver,
-//       message: null,
-//     });
-//   }
-// });
+    // Read row(s) from spreadsheet
+    const getRows = await googleSheets.spreadsheets.values.get({
+      spreadsheetId,
+      range: 'driver-list!A2:A',
+    });
 
-// app.post('/submit', (req, res) => {
-//   const selectedDriver = req.body.driver;
-//   const wankerStatus = req.body.wankerStatus;
+    const typedName = req.body.drivers;
+    let driverNames = getRows.data.values.flat();
 
-//   console.log('Selected Driver:', selectedDriver);
-//   console.log('Wanker Status:', wankerStatus);
+    if (driverNames.includes(typedName)) {
+      res.redirect('/wanker?driver=' + typedName);
+    } else {
+      res.render('home', {
+        driver: null,
+        message: 'Sorry, no match found!',
+      });
+    }
+  } catch (error) {
+    // Handle any errors that occur during authentication or API requests
+    console.error('Error:', error);
+    res.status(500).send('An error occured.');
+  }
+});
 
-//   if (wankerStatus === 'yes') {
-//     res.render('success', {
-//       driver: selectedDriver,
-//       message: wankerMessage,
-//     });
-//   } else if (wankerStatus === 'no') {
-//     res.render('success', {
-//       driver: selectedDriver,
-//       message: goatMessage,
-//     });
-//   }
-// });
+app.get('/wanker', (req, res) => {
+  const driver = req.query.driver;
+  if (!driver) {
+    res.redirect('/');
+  } else {
+    res.render('wanker', {
+      driver: driver,
+      message: null,
+    });
+  }
+});
+
+
+app.post('/submit', async (req, res) => {
+  try {
+    const selectedDriver = req.body.driver;
+    const wankerStatus = req.body.wankerStatus;
+
+    // Authenticate and get the client
+    const client = await authenticate();
+
+    // Instance of Google Sheets API
+    const googleSheets = google.sheets({ version: 'v4', auth: client });
+    // const spreadsheetId = '... spreadsheetId';
+
+    // Read row(s) from spreadsheet
+    const getRows = await googleSheets.spreadsheets.values.get({
+      spreadsheetId,
+      range: 'driver-list!A2:A',
+    });
+
+    const driverNames = getRows.data.values.flat();
+
+    // Write row(s) to spreadsheet
+    await googleSheets.spreadsheets.values.append({
+      spreadsheetId,
+      range: `driver-list!E${driverNames.indexOf(selectedDriver) + 2}`,
+      valueInputOption: 'USER_ENTERED',
+      resource: {
+        values: [[wankerStatus === 'yes' ? wankerMessage : goatMessage]],
+      },
+    });
+
+    console.log('Selected Driver:', selectedDriver);
+    console.log('Wanker Status:', wankerStatus);
+
+    res.render('success', {
+      driver: selectedDriver,
+      message: 'Your RSVP has been sent!',
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('An error occurred.');
+  }
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
